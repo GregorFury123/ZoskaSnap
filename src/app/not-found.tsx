0d97597f93229afca1ecfa4666d1cd8fedf90a69
GregorFury123 <mbig2653@gmail.com>
2024-10-02 13:54:18 +0200

    initial

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+
+// src/app/not-found.tsx
+
+
+import  Typography  from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function NotFound() {
+    
+  return (
+    <Box>
+      <Typography> There's nothing like </Typography>
+    </Box>  
+  );
+}
