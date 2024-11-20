4bfef43aa85408c736c54fd89130dc9aaafd667c
GregorFury123 <mbig2653@gmail.com>
2024-11-20 14:08:31 +0100

    GDPR

--- src/app/gdpr/page.tsx
+++ src/app/gdpr/page.tsx
@@ -2,13 +2,14 @@
 
 
 import  Typography  from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 export const metadata = { title: "GDPR | ZosnkaSnap"}
 
 export default function GDPR() {
-  return (
-  
-      <Typography> Toto je GDPR </Typography>
-        
-  );
-}
+    return (
+      <Container>
+        <Typography> Toto je GDPR </Typography>
+      </Container>   
+    );
+  }
