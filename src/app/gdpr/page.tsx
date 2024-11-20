75930160fb714249a0426f7219e51bed0fa4cb4b
GregorFury123 <mbig2653@gmail.com>
2024-11-20 13:57:20 +0100

    gdpr

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,14 @@
+// src/app/gdpr/page.tsx
+
+
+import  Typography  from "@mui/material/Typography";
+
+export const metadata = { title: "GDPR | ZosnkaSnap"}
+
+export default function GDPR() {
+  return (
+  
+      <Typography> Toto je GDPR </Typography>
+        
+  );
+}
